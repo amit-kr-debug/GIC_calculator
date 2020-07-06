0dbd1ad6853284fc7ef29b239e82244315e5973d
Amit Kumar <amitkr0819@gmail.com>
2020-07-06 20:10:27 +0530

    last year update

--- calculator.py
+++ calculator.py
@@ -115,8 +115,8 @@ def till_mar1998(start_date, end_date):
                 OB += interest
                 Total = interest
                 start_date[2] += 1
-        elif start_date[2] == end_date[2] and end_date[2] > 2003:
-            months = end_date[1] - start_date[1] + 1
+        elif start_date[2] == end_date[2] and end_date[2] > 2010:
+            months = 9 + end_date[1]
             interest = int(round(OB * months * 8 / 1200))
             OB += interest
             Total = interest
@@ -183,7 +183,7 @@ def till_end(start_date, end_date):
             start_date[2] += 1
 
         elif start_date[2] == end_date[2]:
-            months = end_date[1] - start_date[1]
+            months = end_date[1] - start_date[1] +1
             interest = int(round((OB * months + months / 2 * (2 * 21 + (months - 1) * 21)) * 8 / 1200))
             start_date[1] = 1
             Total = 21 * months + interest
